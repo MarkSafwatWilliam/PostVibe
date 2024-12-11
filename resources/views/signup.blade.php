f095934b6fc8b4919bb3e971b0418f9c49a70110
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-12-12 01:02:17 +0200

    Enhance signup page layout and styling; add form inputs with Tailwind CSS utilities

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,5 +1,67 @@
 <x-layout>
-  <main class="min-h-dvh text-center text-4xl">
-    Signup Page
-  </main>
+    <main
+        class="min-h-dvh px-10 sm:px-sm md:px-md lg:px-lg xl:px-xl flex flex-col md:flex-row justify-center md:justify-between items-center gap-10 md:gap-16 lg:gap-20 xl:gap-x-28"
+    >
+        <div class="md:max-w-sm lg:max-w-md">
+            <h1 class="text-primary text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-4">Sign up Today</h1>
+            <p class="text-xl">
+                Create an account so that you’d be able to create Awesome blogs!
+            </p>
+        </div>
+        <div class="w-full md:w-auto md:flex-grow">
+            <form action="/signup" method="post" class="">
+                @csrf
+                <div class="flex flex-col gap-y-[1.75rem]">
+                    <div>
+                        <label for="name" class="text-primary">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            class="form-input"
+                        />
+                    </div>
+                    <div>
+                        <label for="email" class="text-primary">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            class="form-input"
+                        />
+                    </div>
+                    <div>
+                        <label for="password" class="text-primary"
+                            >Password</label
+                        >
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            class="form-input"
+                        />
+                    </div>
+                    <div>
+                        <label for="confirmedpassword" class="text-primary"
+                            >Confirm password</label
+                        >
+                        <input
+                            type="password"
+                            name="confirmedpassword"
+                            id="confirmedpassword"
+                            class="form-input"
+                        />
+                    </div>
+                    <div>
+                        <button
+                            type="submit"
+                            class="bg-primary w-full text-white py-2 px-4 rounded-md"
+                        >
+                            Sign up
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </main>
 </x-layout>
