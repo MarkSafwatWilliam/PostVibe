5f121b30f47a2abb6b4eba4b811cb3e659c1da2d
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-12-12 00:16:23 +0200

    Create signup functionality with user registration; update users table and add signup route and view

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+  <main class="min-h-dvh text-center text-4xl">
+    Signup Page
+  </main>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SignUpController;
+
+
 
 Route::get('/', function () {
   return View('welcome');
 });
+
+Route::get('/signup', [SignUpController::class, 'index']);
