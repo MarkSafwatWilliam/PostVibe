cec6151c41accfb876d10f50767411d70142d819
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-12-19 22:07:36 +0200

    Add mobile menu toggle functionality and improve layout structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import "./bootstrap";
+
+//  To ensure that the DOM is fully loaded before running the script
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.getElementById("menu");
+    const menuToggle = document.getElementById("menu-toggle");
+    const toggleMenu = () => {
+        menu.classList.toggle("hidden");
+    };
+    menuToggle.addEventListener("click", toggleMenu);
+});
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,6 +26,7 @@
             </div>
             <nav class="text-nav text-[1rem] relative">
                 <ul
+                    id="menu"
                     class="absolute w-[130px] top-8 right-2 text-center [&>:not(:last-child)]:border-b [&>:not(:last-child)]:border-white [&>li]:py-2 bg-primary text-white z-40 md:flex md:w-auto md:flex-row md:static md:dpy-0 md:bg-transparent md:text-black items-center gap-5"
                 >
                     <li><a href="/">Home</a></li>
@@ -65,7 +66,7 @@
                     @endauth
                 </ul>
 
-                <button class="md:hidden">
+                <button class="md:hidden" id="menu-toggle">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         x="0px"
@@ -73,7 +74,7 @@
                         width="100"
                         height="100"
                         viewBox="0 0 30 30"
-                        class="fill-current w-6 h-6 text-red-600"
+                        class="fill-current w-6 h-6 text-primary"
                     >
                         <path
                             d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"
